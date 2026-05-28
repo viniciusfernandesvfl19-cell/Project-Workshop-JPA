d10399dcf25437d841fb0fe78de7606d7afd640a
Vinicius Fernandes <vinicius.fernandes.vfl19@gmail.com>
2026-05-28 20:01:17 -0300

    Métodos subTotal e total

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -90,6 +90,14 @@ public class Order implements Serializable {
         return Items;
     }
 
+    public Double getTotal() {
+        double sum = 0;
+        for (OrderItem x : Items) {
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
