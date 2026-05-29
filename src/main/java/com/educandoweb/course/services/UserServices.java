2f372e62a8d86275f0a521b380b59b0a144c62cc
Vinicius Fernandes <vinicius.fernandes.vfl19@gmail.com>
2026-05-28 21:21:42 -0300

    Atualização e deleção de User

--- src/main/java/com/educandoweb/course/services/UserServices.java
+++ src/main/java/com/educandoweb/course/services/UserServices.java
@@ -25,4 +25,20 @@ public class UserServices {
     public User insert(User obj) {
         return repository.save(obj);
     }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(obj);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
 }
