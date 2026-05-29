6ce5c1715e673bbbdbe3ebba2b5c6de4f89841e6
Vinicius Fernandes <vinicius.fernandes.vfl19@gmail.com>
2026-05-28 21:53:17 -0300

    Tratamento de exceção-findById

---
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.educandoweb.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found with id " + id);
+    }
+}
